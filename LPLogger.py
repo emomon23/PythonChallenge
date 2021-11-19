1c7b94b16c4b198b6c47480b2709687a27edc57d
Michael Emo <memo@iemosoft.com>
2021-11-18 18:25:35 -0600

    Added Logger

---
+++ LPLogger.py
@@ -0,0 +1,9 @@
+class Logger:
+    def Log(msg):
+        print(msg);
+
+    def LogError(err):
+        if err == None
+            print("Loger.LogError - err = None");
+
+        print(f"Unexpected {err=}, {type(err)=}")
